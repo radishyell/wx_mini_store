f48a132acb87c4ffe0d07f0d62462c8fed7118ab
radish <597395959@qq.com>
2020-08-04 11:51:17 +0800

    修改分享

--- utils/tools/share.js
+++ utils/tools/share.js
@@ -13,14 +13,10 @@ export default function (options, params = null) {
 		// 点击微信右上角的分享按钮
 	}
 	const defaultConfig = {
-		title: '默认分享，6字 后面变成省略号',  // 26字 后面变成省略号
-		imageUrl: '分享封面。必须是URL地址。URL地址记得是否添加了安全域名',  
+		title: '',  // 默认分享，6字 后面变成省略号
+		imageUrl: '',   // 分享封面。必须是URL地址。URL地址记得是否添加了安全域名
 		path: '/pages/landing/index',      // 分享的后，其他用户点击进来的路径
 	}
-
-	
-	if (params) {
-		Object.assign(defaultConfig, this.data.shareConfig || {}, params);
-	}
+	Object.assign(defaultConfig, this.data.shareConfig || {}, params || {});
 	return defaultConfig;
 }
