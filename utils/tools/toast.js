698fd5eef09feacde2b4a1eb4be9de01062c6f82
radish <597395959@qq.com>
2020-08-07 16:38:39 +0800

    ss

--- utils/tools/toast.js
+++ utils/tools/toast.js
@@ -8,6 +8,7 @@ export default function (title = null, isSuccess = true, duration = 1500) {
 	if (!title) {
 		title = isSuccess ? 'success' : 'fail';
 	}
+	// 显示加载图标，此时 title 文本最多显示 7 个汉字长度
 	const params = { title, duration }
 	if (isSuccess && this.data.toastPath && this.data.toastPath.success) {
 		params.image = this.data.toastPath.success;
@@ -15,6 +16,10 @@ export default function (title = null, isSuccess = true, duration = 1500) {
 		params.image = this.data.toastPath.fail;
 	} else {
 		params.icon = isSuccess ? 'success' : 'none';
+	}
+
+	if (title.length > 7) {
+		params.icon = 'none';
 	}
 	wx.showToast(params);
 }
